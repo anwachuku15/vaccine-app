7660d22a26dcd70767edce86167bc1ec96daf0da
anwachuku15 <anwachuku15@gmail.com>
2021-03-10 15:04:13 -0500

    minor update

--- screens/User/ProfileScreen.js
+++ screens/User/ProfileScreen.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { useColorScheme } from "react-native-appearance";
 
 const ProfileScreen = () => {
+  const colorScheme = useColorScheme();
+  const text = colorScheme === "dark" ? "white" : "black";
+
   return (
     <View style={styles.screen}>
-      <Text>Profile</Text>
+      <Text style={{ color: text }}>Profile</Text>
     </View>
   );
 };
