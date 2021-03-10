71115abf3b9bfd05f7ae0dbd0b37cdb69f54a9ce
anwachuku15 <anwachuku15@gmail.com>
2021-03-10 15:04:20 -0500

    minor update

--- components/ScreenContainer.js
+++ components/ScreenContainer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyleSheet, Text, SafeAreaView } from "react-native";
-
+import Colors from "../constants/Colors";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
