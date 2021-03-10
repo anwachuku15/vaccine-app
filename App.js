259e08073dc0393ec7b95746913343433949a3fe
anwachuku15 <anwachuku15@gmail.com>
2021-03-10 14:59:50 -0500

    updated App navigation

--- App.js
+++ App.js
@@ -1,71 +1,131 @@
 import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect, useLayoutEffect } from "react";
+import { Linking, Platform, ActivityIndicator, View } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { db } from "./Firebase/Fire";
-import { AppearanceProvider } from "react-native-appearance";
-import { NavigationContainer } from "@react-navigation/native";
+import { enableScreens } from "react-native-screens";
+
+import {
+  NavigationContainer,
+  // getFocusedRouteNameFromRoute,
+  // useNavigationState,
+} from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+// import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  createDrawerNavigator,
+  // DrawerContentScrollView,
+  // DrawerItemList,
+  // DrawerItem,
+} from "@react-navigation/drawer";
+// import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { AppearanceProvider, useColorScheme } from "react-native-appearance";
+import { useFonts } from "expo-font";
+import { Montserrat_400Regular } from "@expo-google-fonts/dev";
 
-import ScreenContainer from "./components/ScreenContainer";
+// import BottomTabs from "./navigation/BottomTabs";
+import RootStack from "./navigation/BottomTabStack";
+// import SettingsStack from "./navigation/SettingsStack";
+import DrawerLayout from "./navigation/DrawerLayout";
+import SplashScreen from "./screens/SplashScreen";
 
-import IntroScreen from "./screens/Onboarding/IntroScreen";
-import SignInScreen from "./screens/Onboarding/SignInScreen";
-import CreateAccountScreen from "./screens/Onboarding/CreateAccountScreen";
-import HomeScreen from "./screens/User/DashboardScreen";
-import ProfileScreen from "./screens/User/ProfileScreen";
-import ScheduleScreen from "./screens/User/ScheduleScreen";
-import SettingsScreen from "./screens/User/SettingsScreen";
-import VaccinesScreen from "./screens/User/VaccinesScreen";
-
-import DashboardStack from "./navigation/DashboardStack";
-import ProfileStack from "./navigation/ProfileStack";
+import Colors from "./constants/Colors";
 
 const AuthStack = createStackNavigator();
-const BottomTabs = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
+
+const PERSISTANCE_KEY = "NAVIGATION_STATE";
+
+// enableScreens();
 
 export default function App() {
+  const [isReady, setIsReady] = useState(false);
+  const [initialState, setInitialState] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const restoreState = async () => {
+      try {
+        const initialUrl = await Linking.getInitialURL();
+
+        if (Platform.OS !== "web" && initialUrl == null) {
+          const savedStateString = await AsyncStorage.getItem(PERSISTANCE_KEY);
+          const state = savedStateString
+            ? JSON.parse(savedStateString)
+            : undefined;
+          if (state !== undefined) {
+            setInitialState(state);
+          }
+        }
+      } finally {
+        setIsReady(true);
+      }
+    };
+
+    if (!isReady) {
+      restoreState();
+    }
+  }, [isReady]);
+
+  const colorScheme = useColorScheme();
+  const myTheme = {
+    dark: false,
+    colors: {
+      primary: Colors.blue,
+      background: "black",
+      card: colorScheme === "dark" ? Colors.plum : "white",
+      text: colorScheme === "dark" ? "white" : "black",
+      border: "transparent",
+      notification: Colors.pinkcandy,
+    },
+  };
+
+  const [fontsLoaded] = useFonts({
+    montserrat: Montserrat_400Regular,
+  });
+
+  const DrawerNav = () => {
+    return (
+      <Drawer.Navigator
+        edgeWidth={100}
+        drawerType="slide"
+        drawerContent={(props) => <DrawerLayout {...props} />}
+      >
+        <Drawer.Screen name="Root" component={RootStack} />
+      </Drawer.Navigator>
+    );
+  };
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  if (!isReady) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center" }}>
+        <ActivityIndicator size="large" color={Colors.blue} />
+      </View>
+    );
+  }
+
+  if (!isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center" }}>
+        <ActivityIndicator size="large" color={Colors.blue} />
+      </View>
+    );
+  }
+
   return (
     <AppearanceProvider>
-      <NavigationContainer>
-        <BottomTabs.Navigator>
-          <BottomTabs.Screen
-            name="Dashboard"
-            component={DashboardStack}
-            initialParams={{ firstRoute: "Dashboard" }}
-          />
-          {/* <BottomTabs.Screen
-          name="Schedule"
-          component={DashboardStack}
-          initialParams={{ firstRoute: "Schedule" }}
-        />
-        <BottomTabs.Screen
-          name="Vaccines"
-          component={DashboardStack}
-          initialParams={{ firstRoute: "Vaccines" }}
-        /> */}
-          <BottomTabs.Screen
-            name="Profile"
-            component={ProfileStack}
-            initialParams={{ firstRoute: "Profile" }}
-          />
-        </BottomTabs.Navigator>
-        {/* <AuthStack.Navigator>
-        <AuthStack.Screen name="Intro" component={IntroScreen} />
-        <AuthStack.Screen
-          name="SignIn"
-          component={SignInScreen}
-          options={{ title: "Sign In" }}
-        />
-        <AuthStack.Screen
-          name="CreateAccount"
-          component={CreateAccountScreen}
-          options={{ title: "Create Account" }}
-        />
-      </AuthStack.Navigator> */}
+      <NavigationContainer
+        theme={myTheme}
+        initialState={initialState}
+        onStateChange={(state) =>
+          AsyncStorage.setItem(PERSISTANCE_KEY, JSON.stringify(state))
+        }
+      >
+        <DrawerNav />
       </NavigationContainer>
     </AppearanceProvider>
   );
