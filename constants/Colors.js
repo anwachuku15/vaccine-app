e6a920cdb29ef6e55a5ec90ca3dd45e53a71dfad
anwachuku15 <anwachuku15@gmail.com>
2021-03-10 15:01:13 -0500

    added colors

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,6 +1,12 @@
 export default {
   blue: "#35a7ff",
+  lightblue: "#7DC3F9",
   yellow: "#ffe74c",
   pinkcandy: "#ff5964",
   green: "#6bf178",
+  bluepowder: "#CAF0F8",
+  black: "#000807",
+  plum: "#251119",
+  orange: "#fc4a1a",
+  orellow: "#f7b733",
 };
